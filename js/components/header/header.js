e7c32bf7e58cdd36812dfe4d942aff4aa90d91dc
Sérgio Santos <sergiosantos2612@gmail.com>
2022-01-08 18:51:50 +0000

    Added mobile nav

--- js/components/header/header.js
+++ js/components/header/header.js
@@ -1,1 +1,25 @@
-import './header.css';
+import "./header.css";
+
+const burgerBtn = document.querySelector(".burger__cta");
+const mobileMenu = document.querySelector(".mobile__nav");
+const mobileNavLinks = document.querySelectorAll(".menu__item");
+
+const mobileCloseBtn = document.querySelector(".close__cta");
+
+mobileCloseBtn.addEventListener("click", closeMobileMenu);
+
+function closeMobileMenu() {
+    mobileMenu.classList.remove("mobile__nav--open");
+    mobileMenu.classList.add("mobile__nav--close");
+}
+
+burgerBtn.addEventListener("click", () => {
+  mobileMenu.classList.remove("mobile__nav--close");
+  mobileMenu.classList.add("mobile__nav--open");
+});
+
+mobileNavLinks.forEach((mobileLink) => {
+  mobileLink.addEventListener("click", () => {
+      closeMobileMenu();
+  });
+});
--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
-import './components/header/header';
-import './components/places/places';
-import './components/info/info';
-import './components/reviews/reviews';
-import './components/faqs/faqs';
-import './components/footer/footer';
+import "./components/header/header";
+import "./components/places/places";
+import "./components/info/info";
+import "./components/reviews/reviews";
+import "./components/faqs/faqs";
+import "./components/footer/footer";
+
+const nav = document.querySelector(".nav");
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+  if (lastScrollY < window.scrollY) {
+    nav.classList.add("nav--hidden");
+    nav.classList.add("nav-scroll");
+  } else {
+    nav.classList.remove("nav--hidden");
+  }
+  lastScrollY = window.scrollY;
+
+  if (window.scrollY <= 0) {
+    nav.classList.remove('nav-scroll');
+  }
+});
