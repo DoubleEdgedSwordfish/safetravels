552cad1d3d9c8fe51895f5bd32205bb4971269eb
Sérgio Santos <sergiosantos2612@gmail.com>
2022-01-14 17:50:53 +0000

    Made the reviews sect responsive

--- js/components/reviews/reviews.js
+++ js/components/reviews/reviews.js
@@ -1,1 +1,35 @@
-import './reviews.css';
+import "./reviews.css";
+
+const prevButton = document.querySelector(".prev__button");
+const nextButton = document.querySelector(".next__button");
+const slides = document.querySelectorAll(".reviews__card");
+const totalSlides = slides.length;
+let slidePosition = 0;
+
+/* nextButton.addEventListener("click", moveToNextSlide)
+function moveToNextSlide() {
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+    updateSlidePosition()
+}
+
+function updateSlidePosition() {
+    for (let slide of slides) {
+        slide.classList.remove('card--visible');
+    }
+    slides[slidePosition].classList.add('card--visible')
+}
+
+
+prevButton.addEventListener('click', moveToPrevSlide)
+function moveToPrevSlide() {
+      if (slidePosition === 0 ) {
+        slidePosition = 0;
+    } else {
+        slidePosition--;
+    }
+}
+ */
