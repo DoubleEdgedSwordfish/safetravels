cbdd1b3dc1a21c48afe0f3d20eafe709d6cf5e18
Sérgio Santos <sergiosantos2612@gmail.com>
2022-01-19 18:02:34 +0000

    Added accordion logic

--- js/components/cookies/cookies.js
+++ js/components/cookies/cookies.js
@@ -1,1 +1,18 @@
-import './cookies.css';
+import "./cookies.css";
+
+const cookies = document.querySelector(".cookies");
+const cookiesBtn = document.querySelector(".cookies__cta");
+
+cookiesBtn.addEventListener("click", onCookiesClick);
+
+// Add cookies to the local storage
+function onCookiesClick() {
+    localStorage.setItem("cookies", true);
+    cookies.classList.add("cookies-hide");
+}
+
+if(!localStorage.getItem('cookies')) {
+    setTimeout(() => {
+      cookies.classList.remove("cookies-hide");
+    }, 5000);
+}
--- js/components/faqs/faqs.js
+++ js/components/faqs/faqs.js
@@ -1,11 +1,38 @@
 import "./faqs.css";
 
-const downArrow = document.querySelector(".accordion__down");
+const accordionHeader = document.querySelectorAll(".accordion__header");
 
-downArrow.addEventListener("click", () => {
-  if (!downArrow.classList.contains("accordion__up"))
-    downArrow.classList.add("accordion__up");
-  else if (downArrow.classList.contains) {
-    downArrow.classList.remove("accordion__up");
-  }
+accordionHeader.forEach((header) => {
+  header.addEventListener("click", () => {
+    // If an accordion is already open then close it
+    const isAccordionOpen = document.querySelector('.accordion__header.active');
+    if (isAccordionOpen && isAccordionOpen !== accordionHeader) {
+      isAccordionOpen.classList.toggle('active');
+      isAccordionOpen.nextElementSibling.style.maxHeight = 0;
+    }
+
+    // Open and close an accordio at will
+    header.classList.toggle("active");
+    const accordionContent = header.nextElementSibling;
+    if (header.classList.contains("active")) {
+      accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+    } else {
+      accordionContent.style.maxHeight = 0;
+    }
+  });
 });
+
+// Button rating logic
+const positiveBtn = document.querySelector(".rating__cta.positive");
+const ratingSearch = document.querySelectorAll(".rating__search");
+
+let randomNum = Math.floor(Math.random() * 100);
+ratingSearch.forEach(ratings => {
+  ratings.childNodes.forEach(rating => {
+    /* rating.textContent = `Search ${randomNum} out of ${randomNum * 2 / 1} found this helpful.` */
+  })
+})
+
+positiveBtn.addEventListener('click', () => {
+  console.log("clicked")
+})
