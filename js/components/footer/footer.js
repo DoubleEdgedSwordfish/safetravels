1bacf02c1f0cce4a8600b2a3321ecabc47a97bba
Sérgio Santos <sergiosantos2612@gmail.com>
2022-01-28 12:01:51 +0000

    Added functionality to footer popup

--- js/components/footer/footer.js
+++ js/components/footer/footer.js
@@ -2,12 +2,21 @@ import "./footer.css";
 
 const submitBtn = document.querySelector(".footer__submit");
 const mailInput = document.querySelector(".footer__input");
+const emailPopupMessage = document.querySelector('.popup__description');
 
 submitBtn.addEventListener("click", (e) => {
   if (mailInput.value !== null && mailInput.value !== "") {
     e.preventDefault;
-    alert(`An email has been sent to ${mailInput.value}`);
+    emailPopup.style.display = "flex"
+    emailPopupMessage.textContent = `An email has been sent to ${mailInput.value}`
+    mailInput.value = ""
   } else if (mailInput.value == "") {
     alert("Please insert a valid email");
   }
 });
+
+const closeBtn = document.querySelector('.popup__close');
+const emailPopup = document.querySelector('.footer__popup');
+closeBtn.addEventListener('click', () => {
+  emailPopup.style.display = "none";
+})
